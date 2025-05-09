94eb1c19a13ecf21f8b9bc80abe15ff3f2df0f32
rushi <rushikeshgaikwad000@gmail.com>
2025-05-10 00:52:43 +0530

    Add project files.

---
+++ CodePulse.api/Controller/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.api.Data;
+using CodePulse.api.Models.Domain;
+using CodePulse.api.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private  readonly ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+                
+        }
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategpryRequest request)
+        {
+            //MApt DTO to  Domain class
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+
+            //map Domain to DTO
+            var responce = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(responce);
+        }
+
+    }
+}
---
+++ CodePulse.api/Models/DTO/CreateCategpryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CodePulse.api.Models.DTO
+{
+    public class CreateCategpryRequest
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+    }
+}
